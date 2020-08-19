60734049941e49cd36b7eeb603483c3e11a796b8
Yasser Al-Barbary <Yasser.Al-Barbary@acutest.com>
2020-08-19 22:30:47 +0100

    Add Abouta page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomPageView
+from .views import HomPageView, AboutPageView
 
 urlpatterns = [
     path('', HomPageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
